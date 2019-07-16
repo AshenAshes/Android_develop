e48be3fcb4dfc2b3ebf05c24ff6b544110e2873c
AshenAshes <568976222@qq.com>
2019-07-17 05:09:07 +0800

    day7 permission

---
+++ ch7AndroidStudy/app/src/main/java/com/domker/study/androidstudy/ViewAdapter.java
@@ -0,0 +1,43 @@
+package com.domker.study.androidstudy;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewAdapter extends PagerAdapter {
+    private List<View> datas = new ArrayList<View>();
+
+    @Override
+    public int getCount() {
+        return datas.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = datas.get(position);
+        if (view.getParent() != null)  {
+            ((ViewGroup) view.getParent()).removeView(view);
+        }
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(datas.get(position));
+    }
+
+    public void setDatas(List<View> list) {
+        datas = list;
+        notifyDataSetChanged();
+    }
+}
